2709918b45fab3e207cec286d5e57df5d98f4586
Oleksii Dykan <alick_bass@mail.ru>
2016-08-08 23:05:29 +0200

    Initial Commit

---
+++ SwiftyStoryboard/SwiftyStoryboard.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyStoryboard.h
+//  SwiftyStoryboard
+//
+//  Created by Oleksii on 08/08/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyStoryboard.
+FOUNDATION_EXPORT double SwiftyStoryboardVersionNumber;
+
+//! Project version string for SwiftyStoryboard.
+FOUNDATION_EXPORT const unsigned char SwiftyStoryboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyStoryboard/PublicHeader.h>
+
+
