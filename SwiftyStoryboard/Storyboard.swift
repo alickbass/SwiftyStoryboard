1408cc0533df9d71783395ac27d7c6cd15243048
Oleksii Dykan <alick_bass@mail.ru>
2016-08-08 23:29:27 +0200

    Add storyboard protocol

---
+++ SwiftyStoryboard/Storyboard.swift
@@ -0,0 +1,14 @@
+//
+//  Storyboard.swift
+//  SwiftyStoryboard
+//
+//  Created by Oleksii on 08/08/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+import Foundation
+
+public protocol Storyboard {
+    associatedtype ControllerId: RawRepresentable
+    static var fileName: String { get }
+}
