5cfcabc9c5943a0440a6c71749a545a77f0512f8
Oleksii Dykan <alick_bass@mail.ru>
2016-08-14 16:47:42 +0200

    Add NSStoryboardExtension

---
+++ SwiftyStoryboard/NSStoryboardMethods.swift
@@ -0,0 +1,32 @@
+//
+//  NSStoryboardMethods.swift
+//  SwiftyStoryboard
+//
+//  Created by Oleksii on 14/08/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+import Cocoa
+
+public extension Storyboard {
+    static var storyboard: NSStoryboard {
+        return NSStoryboard(name: Self.fileName, bundle: nil)
+    }
+}
+
+public extension Storyboard where ControllerId.RawValue == String {
+    public static func instantiateViewController<T: NSViewController>(with identifier: ControllerId) -> T {
+        guard let controller = Self.storyboard.instantiateControllerWithIdentifier(identifier.rawValue) as? T else {
+            fatalError("Could not instantiate view controller of given type")
+        }
+        
+        return controller
+    }
+    
+    public static func instantiateInitialController<T: AnyObject>() -> T {
+        guard let controller = Self.storyboard.instantiateInitialController() as? T else {
+            fatalError("Could not instantiate initial controller of given type")
+        }
+        return controller
+    }
+}
